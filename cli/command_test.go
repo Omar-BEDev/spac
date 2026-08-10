7f6d85dc1042a3f0a4c50fd8d7fff30a39abb0cb
Omar-BEDev <kaliochibrahim@gmail.com>
2026-08-10 01:44:59 +0000

    chore(command): add new edge cases of check command list

--- cli/command_test.go
+++ cli/command_test.go
@@ -9,8 +9,66 @@ func TestIsAvailableCommand(t *testing.T) {
 		name     string
 		excepted bool
 	}{
-		{name: "post", excepted: true},
-		{name: "", excepted: false},
+		{
+			name:     "post",
+			excepted: true,
+		},
+		{
+			name:     "get",
+			excepted: true,
+		},
+		{
+			name: "put", 
+			excepted: true,
+		},
+		{
+			name: "delete", 
+			excepted: true,
+		},
+		{
+			name: "new Req", 
+			excepted: true,
+		},
+		{
+			name: "", 
+			excepted: false,
+		},
+		{
+			name: "POST", 
+			excepted: false,
+		},
+		{
+			name: " post", 
+			excepted: false,
+		},
+		{
+			name: "post ", 
+			excepted: false,
+		},
+		{
+			name: "new req", 
+			excepted: false,
+		},
+		{
+			name: "new Req ", 
+			excepted: false,
+		},
+		{
+			name: "gert", 
+			excepted: false,
+		},
+		{
+			name: "api", 
+			excepted: false,
+		},
+		{
+			name: "method", 
+			excepted: false,
+		},
+		{
+			name: "cmd", 
+			excepted: false,
+		},
 	}
 	for _, tt := range tests {
 		result := IsAvailableCommand(tt.name)
